05c4242d39517d75d65ca8b6b23d006797e5243f
Julius <74841960+JuliusZhou124@users.noreply.github.com>
2025-09-20 22:51:43 -0400

    Write endpoints for menu station and item nutrients

--- backend_test.py
+++ backend_test.py
@@ -3,7 +3,7 @@ import requests
 import json
 from datetime import date
 
-async def fetch_and_convert(location: str, date: date) -> dict:
+async def fetch_and_convert_by_meal_date(location: str, date: date) -> dict:
     url = f'https://api.hfs.purdue.edu/menus/v2/locations/{location}/{date.strftime("%Y-%m-%d")}'
     # print(date.strftime("%Y-%m-%d"))
 
@@ -22,11 +22,10 @@ async def fetch_and_convert(location: str, date: date) -> dict:
         json_string = json.dumps(json_data, indent=4)
 
         # Print the formatted JSON data
-        # print(json_string)
 
-        # Optionally, save the JSON data to a file
-        with open("output.json", "w") as json_file:
-            json_file.write(json_string)
+        # # Optionally, save the JSON data to a file
+        # with open("output.json", "w") as json_file:
+        #     json_file.write(json_string)
 
         return json_data
 
@@ -35,9 +34,32 @@ async def fetch_and_convert(location: str, date: date) -> dict:
     except Exception as e:
         print(f"Error processing data: {e}")
 
-def main():
-    data = fetch_and_convert()
-    print(data["Location"])
+async def fetch_by_item_id(item_id: str) -> dict:
+    url = f'https://api.hfs.purdue.edu/menus/v2/items/{item_id}'
 
-if __name__ == "__main__":
-    main()
+    try:
+        # Fetch the JSON data from the endpoint
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for HTTP issues
+
+        # Print the Content-Type of the response
+        print("Content-Type:", response.headers.get("Content-Type"))
+
+        # Load the JSON data
+        json_data = response.json()
+
+        # Convert the JSON data to a formatted string for readability
+        return json_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+    except Exception as e:
+        print(f"Error processing data: {e}")
+
+
+# def main():
+#     data = fetch_and_convert_by_meal_date()
+#     print(data["Location"])
+
+# if __name__ == "__main__":
+#     main()
--- main.py
+++ main.py
@@ -1,46 +1,56 @@
 from datetime import date, datetime
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
-from backend_test import fetch_and_convert
+from backend_test import fetch_and_convert_by_meal_date, fetch_by_item_id
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-
 @app.get("/")
 async def read_root() -> dict:
-  
     try:
-        data = await fetch_and_convert("Hillenbrand", date=date(2025, 9, 21))
+        data = await fetch_and_convert_by_meal_date("Hillenbrand", date=date(2025, 9, 21))
         return data
     except Exception as e:
         return {"error": str(e)}
+
+@app.get("/menus/item/{item_id}")
+async def get_item(item_id: str) -> dict:
+    print("this is called")
+    # Get a specific menu item by its UUID within a location + date menu.
+    try:
+        data = await fetch_by_item_id(item_id) 
+        return data
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/menus/{location}/{date}")
 async def read_menu(location: str, date: str) -> dict:
     format_code = "%Y-%m-%d"
     dateObj = datetime.strptime(date, format_code).date()
     try:
-        data = await fetch_and_convert(location, dateObj)
+        data = await fetch_and_convert_by_meal_date(location, dateObj)
         return data
     except Exception as e:
         return {"error": str(e)}
 
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/menus/{location}/{date}/{meals}/stations")
+async def get_menu_stations(location: str, date: str, meals: str) -> dict: 
+    # Get all stations for a specific location + date menu.
+    format_code = "%Y-%m-%d"
+    date_obj = datetime.strptime(date, format_code).date()
+    try:
+        data = await fetch_and_convert_by_meal_date(location, date_obj)
+        mealSlice: list = list(filter(lambda meal: meal["Type"] == meals, data["Meals"]))
+        # print([stations["Name"] for stations in mealSlice[0]["Stations"]])
+        return {
+            "Location": location,
+            "Date": date,
+            "Stations": [stations["Name"] for stations in mealSlice[0]["Stations"]]
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
